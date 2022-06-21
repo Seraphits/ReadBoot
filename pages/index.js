01ec741a0752d930d9ed7fde34e2071d887e52d0
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-06-20 20:25:22 -0400

    H2

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import styled from 'styled-components'
+import { Logo } from '../Components/01-Atoms/Logo'
 
 const Header = styled.div`
   background-color: #0A1108E3;
@@ -14,35 +15,81 @@ const Header = styled.div`
   height: 75vh;
 `
 const Title = styled.h1`
-  /* background-color: aqua; */
+  /* background-color:yellowgreen ; */
   font-size: 6rem;
   width: 100%;
   text-align: left;
-  padding-left: 5rem;
+  padding-left: 8%;
+  text-shadow: .05em .05em .08em black, .08em .08em .1em white ;
 
+  @media only screen and (max-width: 700px) {
+    /* background-color: lightblue; */
+    font-size: 5rem;
+    padding-left: 5%;
+  }
+  @media only screen and (max-width: 500px) {
+    /* background-color: plum; */
+    font-size: 3.5rem;
+  }
   span {
     /* background-color: beige; */
     display: block;
     font-size: 3rem;
     color: white;
+    @media only screen and (max-width: 700px) {
+      /* background-color: blue; */
+      font-size: 2rem;
+    }
+    @media only screen and (max-width: 700px) {
+      /* background-color: teal; */
+      font-size: 1.5rem;
+    }
   }
 `
-const Logo = styled.div`
-  /* background-color: peru; */
-  color: red;
+const Main = styled.main`
+  /* background-color: rebeccapurple; */
+  /* background-image: url(/ReadBoot.png);
+  background-repeat: no-repeat;
+  background-position: right;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 75vh; */
+  padding-top: 3rem;
+`
+
+const CenterHeaderH2 = styled.div`
+  /* background-color:yellowgreen ; */
+  font-size: 3rem;
+  width: 100%;
+  text-align: center;
+  padding-left: 8%;
+  /* text-shadow: .05em .05em .08em black, .08em .08em .1em white ; */
+
+  @media only screen and (max-width: 700px) {
+    /* background-color: lightblue; */
+    font-size: 2.5rem;
+    padding-left: 5%;
+  }
+  @media only screen and (max-width: 500px) {
+    /* background-color: plum; */
+    font-size: 2rem;
+  }
   span {
-   display: inline-block;
-   color: white;
-   font-size: inherit;
+    /* background-color: beige; */
+    display: block;
+    font-size: 2rem;
+    /* color: white; */
+    @media only screen and (max-width: 700px) {
+      /* background-color: blue; */
+      font-size: 1.5rem;
+    }
+    @media only screen and (max-width: 700px) {
+      /* background-color: teal; */
+      font-size: 1.5rem;
+    }
   }
 `
-// const Header = styled.span`
-//   background-color: red;
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-//   height: 75vh;
-// `
 
 export default function Home() {
   return (
@@ -58,15 +105,15 @@ export default function Home() {
           <span>Rebooting Education</span>
         </Title>
       </Header>
-      <main >
-        <h2>
+      <Main >
+        <CenterHeaderH2>
           <span>Check Out Our Classes</span>
-          <span>Education for the 22nd Century</span>
-        </h2>
+          Education for the 22nd Century
+        </CenterHeaderH2>
         <div >
           <div>Classes Card loop through</div>
         </div>
-      </main>
+      </Main>
 
       <footer >
 
