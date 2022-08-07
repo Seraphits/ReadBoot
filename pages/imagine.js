6c1c74fb898cedc233a963b911b30b9cdd4f286e
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-08-06 23:42:24 -0400

    add Imagine page

---
+++ pages/imagine.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Link from 'next/link';
+
+
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Imagine</title>
+        <meta name='keywords' content='ReadBoot'/>
+      </Head>
+     <h1>Imagine</h1>
+    </div>
+  )
+}
