5430b9042626baa28feb87295eb9dc7d0137389c
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-08-12 15:56:51 -0400

    Grid1

--- pages/classes/index.js
+++ pages/classes/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
-import Link from 'next/link';
-import LayersButton from '../../Components/01-Atoms/buttons/3LayerBtm';
+// import Link from 'next/link';
+// import LayersButton from '../../Components/01-Atoms/buttons/3LayerBtm';
 import ClassesTypeButtons from '../../Components/02-Molecules/ButonSets/ClassType';
+// import ClassesGrid from '../../Components/03-Organisms/ClassGrid';
+import { CardFlexGrid } from '../../Components/04-Templetes/flex';
+import ClassCard from '../../Components/02-Molecules/Cards/ClassCards'
 // import { GridAutoFill } from '../../Components/04-Templetes/grids';
 import styles from '../../Components/05-Page/Classes/Classes.module.scss'
 
@@ -18,6 +21,19 @@ export default function Classes() {
         <h1>Classes</h1>
         <ClassesTypeButtons/>
       </header>
+      <h2>All Classes</h2>
+      <CardFlexGrid>
+        <ClassCard  name="Colors Of Learning"
+                    description="Colors of different learning types"
+
+        />
+        <ClassCard/>
+        <ClassCard/>
+        <ClassCard/>
+        <ClassCard/>
+        <ClassCard/>
+        <ClassCard/>
+      </CardFlexGrid>
       </div>
     </div>
   )
