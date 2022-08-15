4bd61e8ac9a27a29ce9593491f3d99cbb75de33b
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-08-15 17:17:07 -0400

    Tech Pages

--- Components/02-Molecules/Cards/PicTitleCard.js
+++ Components/02-Molecules/Cards/PicTitleCard.js
@@ -9,32 +9,22 @@ const Card =  styled.div`
   width: fit-content;
   box-shadow: 0 30px 30px rgba(0,0,0,0.5);
   text-align: center;
-  ${'' /*
-  background-image: url("/images/MainPages/Girl-Holding-World.jpg");
-  position: relative;
-
-
-
-  background-repeat: no-repeat;
-  align-items: center;
-  background-position: center;
-  background-size: cover; */}
+  border-radius: 1rem;
 `
 const PicBox =  styled.div`
   position: relative;
   width: 300px;
   height: 200px;
-
 `
 
 export default function PicTitle(props) {
   return (
-    <Link href={`/classes/${props.id}`}>
+    <Link href={`${props.href}`}>
       <Card>
         <PicBox>
           <Image src={`${props.imgUrl}`} layout='fill' />
         </PicBox>
-        <h1>{props.name}</h1>
+        <h3>{props.name}</h3>
       </Card>
     </Link>
   )
--- pages/classes/choosing-your-technology/index.js
+++ pages/classes/choosing-your-technology/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link';
+import PicTitle from '../../../Components/02-Molecules/Cards/PicTitleCard';
+import { CardFlexGrid } from '../../../Components/04-Templetes/flex';
 
 
 
@@ -10,11 +12,33 @@ export default function Tech() {
         <title>Technologies for Teaching Online</title>
         <meta name='keywords' content='ReadBoot'/>
       </Head>
-     <h1>Technologies for Teaching Online</h1>
-     <p>Technology opens the door to some exciting new ways to learn new things. As an online instructor, you well be using many of the the following types of technologies.</p>
-     <Link href="/classes/choosing-your-technology/video-chat">
+      <h1>Technologies for Teaching Online</h1>
+      <p>Technology opens the door to some exciting new ways to learn new things. As an online instructor, you well be using many of the the following types of technologies.</p>
+     {/* <Link href="/classes/choosing-your-technology/video-chat">
         <a  >Video Chat</a>
-      </Link>
+      </Link> */}
+      <CardFlexGrid>
+        <PicTitle imgUrl='/images/Classes/Build/ChoosingYourTechnology/MultimediaDiscussion/multimedia-discussion.jpg'
+          name="Video Chat"
+          href="/classes/choosing-your-technology/video-chat"
+        />
+        <PicTitle imgUrl='/images/Classes/Build/ChoosingYourTechnology/MultimediaDiscussion/multimedia-discussion.jpg'
+          name="Video Production"
+          href="/classes/choosing-your-technology/video-production"
+        />
+        <PicTitle imgUrl='/images/Classes/Build/ChoosingYourTechnology/MultimediaDiscussion/multimedia-discussion.jpg'
+          name="Video Sharing"
+          href="/classes/choosing-your-technology/video-sharing"
+        />
+        <PicTitle imgUrl='/images/Classes/Build/ChoosingYourTechnology/MultimediaDiscussion/multimedia-discussion.jpg'
+          name="Multimedia Discussion"
+          href="/classes/choosing-your-technology/multimedia-discussion"
+        />
+        <PicTitle imgUrl='/images/Classes/Build/ChoosingYourTechnology/MultimediaDiscussion/multimedia-discussion.jpg'
+          name="Presentation"
+          href="/classes/choosing-your-technology/presentation"
+        />
+      </CardFlexGrid>
     </div>
   )
 }
