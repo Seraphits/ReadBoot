3f3b9c62fe740abe8c05211ed60a250ddad2c534
Seraphits <97304529+Seraphits@users.noreply.github.com>
2022-09-13 02:12:23 -0400

    7

--- pages/classes/benefits-and-challenges-of-teaching-online/benefits/index.js
+++ pages/classes/benefits-and-challenges-of-teaching-online/benefits/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-// import { Grid3 } from "@/Templetes/grids";
-// import PicTitle from "@/Molecules/Cards/PicTitleCard";
+import { Grid3 } from "@/Templetes/grids";
+import PicTitle from "@/Molecules/Cards/PicTitleCard";
 // import { getAllBenfits } from "@/Data/benefits-and-challenges-of-teaching-online/Benefits";
 // import Image from "next/image";
 import Head from "next/head";
@@ -15,7 +15,7 @@ const Benefits = () => {
         <meta name="keywords" content="ReadBoot" />
       </Head>
       <h1>benefits</h1>
-      {/* <p>
+      <p>
         These technologies will allow you to provide face-to-face instruction to
         your learners and simulate a classroom setting. Different applications
         will include different affordances and constraints for instruction.
@@ -24,10 +24,10 @@ const Benefits = () => {
         <b>Click on each one to learn more.</b>
       </p>
       <Grid3>
-         {cards.map((card) => (
+         {/* {cards.map((card) => (
           <PicTitle key={card.ID} title={card.name} imgUrl={card.imgUrl} />
-        ))}
-      </Grid3> */}
+        ))} */}
+      </Grid3>
     </div>
   );
 };
