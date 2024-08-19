a7d5bce6e6b300d61903f78d1985ba3051724811
Seraphits <97304529+Seraphits@users.noreply.github.com>
2024-08-19 10:42:50 -0400

    Remove class not made

---
+++ pages/classes/readboots-six-pillars-reading/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Link from 'next/link';
+
+
+
+export default function Oversee() {
+  return (
+    <div >
+      <Head>
+        <title>ReadBoot's Six Pillars: Reading</title>
+        <meta name='keywords' content="ReadBoot | ReadBoot's Six Pillars: Reading"/>
+      </Head>
+      <header>
+        <h1>ReadBoot's Six Pillars: Reading</h1>
+
+      </header>
+     <h1>Oversee</h1>
+    </div>
+  )
+}
+
